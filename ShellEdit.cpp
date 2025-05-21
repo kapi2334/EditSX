a4382229787dbecce8f7874eb30e39362b7d8811
kapi2334 <kapi2334@gmail.com>
2025-05-21 22:35:08 +0200

    Added operating system checks.

--- ShellEdit.cpp
+++ ShellEdit.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <Windows.h>
+#include <minwindef.h>
+#include "Window.h"
 
+#if defined(_WIN64) || defined(_WIN32)
 
-int main(int argc, char* argv[]){
-    std::cout << "Hello world!";
+    int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR,int nCmdShow){
+        
+        return 1; 
 
-}
+    }
+
+#elif defined(__linux__)
+#elif defined(__APPLE__) && defined(__MACH__)
+#endif        
---
+++ Window.h
@@ -0,0 +1,13 @@
+//Window logic handling f.e. drawing, getting keys etc.
+#include <string>
+namespace sxEditCore{
+
+    class WindowLogic {
+        public:
+            
+
+
+    };
+
+
+}
