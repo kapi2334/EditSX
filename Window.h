3672c7fbc3abb83d8d1f0aab86e355a71474935b
kapi2334 <kapi2334@gmail.com>
2025-05-22 23:52:01 +0200

    Added registering window class

--- Window.h
+++ Window.h
@@ -1,9 +1,29 @@
 //Window logic handling f.e. drawing, getting keys etc.
-#include <string>
+#include <windows.h>
+#include <winuser.h>
 namespace sxEditCore{
 
-    class WindowLogic {
+    class WindowsWindowLogic {
         public:
+            LRESULT CALLBACK Procedure(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
+                //Window message loop
+                switch(uMsg){
+                    case WM_DESTROY:
+                        PostQuitMessage(0);
+                        return 0;
+                    default:
+                        return DefWindowProc(hwnd,uMsg,wParam,lParam);
+                }
+
+            }
+            
+            WNDCLASS RegisterWindow(LRESULT Proc, HINSTANCE WinHandle, const char WinName[]){
+                WNDCLASS wclass = {};
+                wclass.lpfnWndProc = Proc;
+                wclass.hInstance = WinHandle;
+                wclass.lpszClassName = WinName;
+                return wclass;
+            }
             
 
 
