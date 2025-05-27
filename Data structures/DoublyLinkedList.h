bbedcd6d2d610af757d876b107787456858ad969
kapi2334 <kapi2334@gmail.com>
2025-05-27 23:31:14 +0200

    Added a "skeleton" of the caching function

--- Data structures/DoublyLinkedList.h
+++ Data structures/DoublyLinkedList.h
@@ -1,33 +1,68 @@
 
+#include <cstdlib>
 namespace sxEditCore::dataStructures{
 
 
-   class dlList{
-       private:
-       //Single node declaration
-       typedef struct dlNode{
-            char value;
-            struct dlNode* next;
-            struct dlNode* prev;
-       } dlNode; 
-       
-       //Cahed node declaration
-       typedef struct dlCahedNode{
-           int index;
-           dlNode *nodeAddress;
-       } dlCahedNode; 
+    class dlList{
+        private:
+            //Single node declaration
+            typedef struct dlNode{
+                char value;
+                struct dlNode* next;
+                struct dlNode* prev;
+            } dlNode; 
 
-       int size; //Acual size of this list
-       dlNode *First; //Pointer to first node (head)
-       dlNode *Last; //Pointer to last node (tail) 
+            //Cahed node declaration
+            typedef struct dlCachedNode{
+                int index = -1;
+                dlNode *nodeAddress= nullptr;
+            } dlCachedNode; 
+
+            //TO DO: CALCULATING THE THERSHOLD VALUE DEPENDING ON THE SIZE VARIABLE
+            int cacheTreshold = 20;
+            dlCachedNode cachedNode; 
+
+            int size; //Acual size of this list
+            dlNode *First; //Pointer to first node (head)
+            dlNode *Last; //Pointer to last node (tail) 
 
 
+            /*It is used to obtain a node at a specific position - using the node stored in memory.
+             *Returns nullptr when cashing attempt failed - fe. if CahedNode is empty, 
+             or if targetted node and cahed one are too away.
+             * */
+            dlNode* tryToCahe(int nodeIndex){
+                if(cachedNode.nodeAddress == nullptr ) return nullptr; //If cahed node doesnt exist
+                if(nodeIndex >= size || nodeIndex < 0) return nullptr; //If index is outside the array
+                    if(std::abs(nodeIndex - cachedNode.index) < cacheTreshold){ //Success - finding node using one in cashe can be done.
+                        //Cashe types
+                        
+                        if(nodeIndex - cachedNode.index > 0 ){
+                            //Iterating to node from cached one using next property.
+                        }
+                        else if(nodeIndex - cachedNode.index == 0 ){
+                        
+                            //Requested node its that one in the cache
+                            return cachedNode.nodeAddress;
+                        }
+                        else if(nodeIndex - cachedNode.index < 0 ){
 
-       public:
+                            //Iterating to node from cahed one using prev property.
+
+                        }
+
+                    }
+                return nullptr;        
+                
+                
+            }
+
+
+        public:
             dlList(){
-               size = 0; 
-               First = nullptr;
-               Last = nullptr;
+                size = 0; 
+                First = nullptr;
+                Last = nullptr;
             }
             //Adds value (input) to front of the List
             int pushFront(char input){
@@ -53,12 +88,13 @@ namespace sxEditCore::dataStructures{
                     Last = newNode;
                     return 0;
                 }
-                
+
                 newNode -> prev = Last;
                 Last -> next = newNode;
-                
+                Last = newNode;
+
             }
 
-   }; 
+    }; 
 
 }
